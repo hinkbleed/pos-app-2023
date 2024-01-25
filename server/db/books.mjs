c02a4106c14f355b2e14bdae0d79811ea9f0b499
Alejandro <carlosartdev@gmail.com>
2024-01-24 23:18:22 -0600

    entering the void

---
+++ server/db/books.mjs
@@ -0,0 +1,25 @@
+export let booksDB = {
+
+    "libros": [
+        {
+            "name": "Dinosaurios y Vida Prehistórica",
+            "barcode": 45346685646464,
+            "price": 1150,
+            "type": "book",
+            "providor": "Hugo",
+            "editorial": "DK",
+            "year": 2017,
+            "authors": ["Kresley Donney"]
+        },
+        {
+            "name": "Artistas, su vida y sus obras",
+            "barcode": 9781465478764,
+            "price": 450,
+            "type": "book",
+            "providor": "Hugo",
+            "editorial": "DK",
+            "year": 2018,
+            "authors": ["Karen Self", "Jonathan Metcalf"]
+        }
+    ]
+}
